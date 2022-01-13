926f6c8bf4f3fa48fd6d25fa9fa5c5a700adac94
krod2016 <katrinajrodriguez1993@gmail.com>
2022-01-12 21:16:06 -0600

    added index.js, updated route files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,38 +1,26 @@
 INSERT INTO department (name)
-VALUES ("Curators"),
-INSERT INTO department (name)
-VALUES ("Educators"),
-INSERT INTO department (name)
-VALUES ("Docents"),
-INSERT INTO department (name)
-VALUES ("Legal Team");
+VALUES
+('Curators'),
+('Educators'),
+('Docents'),
+('Legal Team');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Curator", 65000, 1),
-INSERT INTO role (title, salary, department_id)
-VALUES ("Educator", 57500, 2),
-INSERT INTO role (title, salary, department_id)
-VALUES ("Docent", 50000, 3),
-INSERT INTO role (title, salary, department_id)
-VALUES ("Lawyer", 130000, 4);
+VALUES
+('Curator', 65000, 1),
+('Educator', 57500, 2),
+('Docent', 50000, 3),
+('Lawyer', 130000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Katrina", "Rodgers", 1, null),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jacob", "Jimenez", 2, null),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Straham", 3, null),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Terry", "Jones", 4, null),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kayle", "Rodriguez", 1, 001),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Zayne", "Johnson", 2, 002),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Grace", "Ramos", 2, 002),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Amber", "Ingram", 3, 003),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Matt", "Westbrook", 3, 003),
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Caroline", "Long", 4, 004);
+VALUES
+('Katrina', 'Rodgers', 1, null),
+('Jacob', 'Jimenez', 2, null),
+('John', 'Straham', 3, null),
+('Terry', 'Jones', 4, null),
+('Kayle', 'Rodriguez', 1, 001),
+('Zayne', 'Johnson', 2, 002),
+('Grace', 'Ramos', 2, 002),
+('Amber', 'Ingram', 3, 003),
+('Matt', 'Westbrook', 3, 003),
+('Caroline', 'Long', 4, 004);
